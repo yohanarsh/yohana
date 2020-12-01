611b5949ea183eebc5a142a15e1cdddf74ca71c3
yohanarsh <75101982+yohanarsh@users.noreply.github.com>
2020-12-01 09:31:26 +0800

    Add files via upload

---
+++ data/qgis1_1.js
@@ -0,0 +1,8 @@
+var json_qgis1_1 = {
+"type": "FeatureCollection",
+"name": "qgis1_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 106.999948139142305, -6.237671608943068 ] } }
+]
+}
